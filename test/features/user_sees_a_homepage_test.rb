6020d47e51e3dbdbac864c85be1b0d67cd9b510d
Bailey Diveley <bailey.diveley@gmail.com>
2018-09-24 13:02:56 -0600

    Create error and about pages

--- test/features/user_sees_a_homepage_test.rb
+++ test/features/user_sees_a_homepage_test.rb
@@ -7,4 +7,18 @@ class HomepageTest < CapybaraTestCase
     assert page.has_content?("Welcome!")
     assert_equal 200, page.status_code
   end
+
+  def test_user_can_see_an_error_msg
+    visit '/hi_there'
+
+    assert page.has_content?("Page not found.")
+    assert_equal 404, page.status_code
+  end
+
+  def test_user_sees_about_page
+    visit '/about'
+
+    assert page.has_content?("About Me!\nHere's some stuff to know.")
+    assert_equal 200, page.status_code
+  end
 end
